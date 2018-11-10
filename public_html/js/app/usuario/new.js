6f417cf6e8c0310952ad00f588a1ec499e2e7cc2
Ramón Lozano <niksokupa@gmail.com>
2018-11-11 00:12:47 +0100

    New user acabadito.

--- public_html/js/app/usuario/new.js
+++ public_html/js/app/usuario/new.js
@@ -1,21 +1,22 @@
 "use strict";
 
 moduleUsuario.controller("usuarioNewController", [
-  "$scope",
-  "$http",
-  "$routeParams",
-  "toolService",
-  function ($scope, $http, $routeParams, toolService) {
-    $scope.create = function () {
+    "$scope",
+    "$http",
+    "$routeParams",
+    "toolService",
+    function ($scope, $http, $routeParams, toolService) {
+        $scope.created = true;
+        $scope.create = function () {
             var json = {
-        id: $scope.id,
-        dni: $scope.dni,
-        nombre: $scope.nombre,
-        ape1: $scope.ape1,
-        ape2: $scope.ape2,
-        login: $scope.login,
-        pass: $scope.pass,
-        id_tipoUsuario: $scope.obj_tipoUsuario_id
+                id: $scope.id,
+                dni: $scope.dni,
+                nombre: $scope.nombre,
+                ape1: $scope.ape1,
+                ape2: $scope.ape2,
+                login: $scope.login,
+                pass: $scope.pass,
+                id_tipoUsuario: $scope.obj_tipoUsuario_id
             }
 
             $http({
@@ -24,21 +25,14 @@ moduleUsuario.controller("usuarioNewController", [
                     'Content-Type': 'application/json;charset=utf-8'
                 },
                 url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=create',
-                params: { json: JSON.stringify(json) }
+                params: {json: JSON.stringify(json)}
             }).then(function (response, data) {
-                console.log(data)
+                $scope.created = false;
             }, function (response) {
                 $scope.status = response.status;
                 $scope.ajaxDataUsuarios = response.data.message || 'Request failed';
             });
         }
-
-
-
-
         $scope.isActive = toolService.isActive;
-
-
-
-  }
+    }
 ]);
