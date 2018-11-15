8b4708107ef5ee9b71d3b3e67abaf69a862a2c6d
a044525499y <a044525499y@s09pc05>
2018-11-15 20:42:40 +0100

    creacion de ficheros comunes base para producto, tipoproducto y factura

---
+++ public_html/js/app/tipoproducto/remove.js
@@ -0,0 +1,40 @@
+"use strict";
+
+moduleTipousuario.controller('tipousuarioRemoveController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 'sessionService',
+    function ($scope, $http, $location, toolService, $routeParams, oSessionService) {
+        $scope.deleted = true;
+        $scope.logged = false;
+        $http({
+            method: "GET",
+            url: `http://localhost:8081/trolleyes/json?ob=tipousuario&op=get&id=${$routeParams.id}`
+        }).then(function (response) {
+            $scope.ajaxData = response.data.message;
+        });
+
+        $scope.eliminar = function () {
+            $http({
+                method: "GET",
+                url: `http://localhost:8081/trolleyes/json?ob=tipousuario&op=remove&id=${$routeParams.id}`
+            }).then(function (response) {
+                $scope.deleted = false;
+            })
+        }
+
+        if (oSessionService.getUserName() !== "") {
+            $scope.loggeduser = oSessionService.getUserName();
+            $scope.logged = true;
+        }
+
+        $scope.logout = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=usuario&op=logout'
+            }).then(function () {
+                $location.url('/');
+            });
+        }
+
+        $scope.isActive = toolService.isActive;
+    }
+
+]);
