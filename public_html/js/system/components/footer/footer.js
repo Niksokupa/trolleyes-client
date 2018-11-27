be597cb7661c4c3a264bd53db5481af9f2aa2ccb
a044525499y <a044525499y@s09pc05>
2018-11-27 16:46:25 +0100

    footer "arreglado" xD

---
+++ public_html/js/system/components/footer/footer.js
@@ -0,0 +1,15 @@
+moduleComponent.component('footerComponent', {
+    //restrict: 'A',
+    templateUrl: 'js/system/components/footer/footer.html',
+    bindings: {
+        // data: '=',
+        // tabla: '<'
+    },
+    controllerAs: 'c',
+    controller: js
+});
+
+function js(){
+    
+}
+
