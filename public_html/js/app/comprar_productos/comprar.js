18ef800ff77196ae467bf239229a98e3cabdd7d9
Ramón Lozano <niksokupa@gmail.com>
2018-12-02 20:13:06 +0100

    total del carrito

--- public_html/js/app/comprar_productos/comprar.js
+++ public_html/js/app/comprar_productos/comprar.js
@@ -1,4 +1,4 @@
-moduleComprarProducto.controller('comprarPlistController', ['$scope', '$http', '$location', 'toolService', '$routeParams', "sessionService", "countcarritoService","$mdDialog",
+moduleComprarProducto.controller('comprarPlistController', ['$scope', '$http', '$location', 'toolService', '$routeParams', "sessionService", "countcarritoService", "$mdDialog",
     function ($scope, $http, $location, toolService, $routeParams, sessionService, countcarritoService, $mdDialog) {
 
         $scope.carritoVacio = false;
@@ -8,6 +8,17 @@ moduleComprarProducto.controller('comprarPlistController', ['$scope', '$http', '
         }).then(function (response) {
             if (response.data.message != null) {
                 $scope.productos = response.data.message;
+                $scope.total = 0;
+                var auxCant = 0;
+                var auxPrecio = 0;
+                var totalInicial = 0;
+                for (var i = 0; i < response.data.message.length; i++) {
+                    auxCant = response.data.message[i].cantidad;
+                    auxPrecio = response.data.message[i].obj_producto.precio;
+
+                    totalInicial += auxCant * auxPrecio;
+                }
+                $scope.total = Math.round(totalInicial * 100) / 100;
             } else {
                 $scope.carritoVacio = true;
             }
@@ -23,6 +34,16 @@ moduleComprarProducto.controller('comprarPlistController', ['$scope', '$http', '
             }).then(function (response) {
                 if (response.data.message != null) {
                     $scope.productos = response.data.message;
+                    var auxCant = 0;
+                    var auxPrecio = 0;
+                    var totalModificado = 0;
+                    for (var i = 0; i < response.data.message.length; i++) {
+                        auxCant = response.data.message[i].cantidad;
+                        auxPrecio = response.data.message[i].obj_producto.precio;
+
+                        totalModificado += auxCant * auxPrecio;
+                    }
+                    $scope.total = Math.round(totalModificado * 100) / 100;
                 } else {
                     $scope.carritoVacio = true;
                 }
@@ -44,17 +65,17 @@ moduleComprarProducto.controller('comprarPlistController', ['$scope', '$http', '
             });
         }
 
-                //Este mensaje se puede mejorar, buscar info en la api oficial de angular material
+        //Este mensaje se puede mejorar, buscar info en la api oficial de angular material
         //https://material.angularjs.org/latest/api/service/$mdDialog
         //https://ajax.googleapis.com/ajax/libs/angular_material/1.1.8/angular-material.css
         $scope.showAlert = function (titulo, description) {
             $mdDialog.show(
-                $mdDialog.alert()
+                    $mdDialog.alert()
                     .clickOutsideToClose(false)
                     .title(titulo)
                     .textContent(description)
                     .ariaLabel('Alert Dialog Demo')
                     .ok('OK!')
-            );
+                    );
         };
     }]);
